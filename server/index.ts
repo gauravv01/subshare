5d4dfc16d38ff02e673d630f11f67a7cc4dba4f2
gaurav <vermagaurav171995@gmail.com>
2025-05-12 20:08:30 +0530

    changes

--- server/index.ts
+++ server/index.ts
@@ -36,13 +36,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.options('*', cors());
 app.use(cors({
-  origin: true, 
+  origin: 'https://subshare-xonz.vercel.app/',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-  exposedHeaders: ['set-cookie'],
-  optionsSuccessStatus: 200
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 
 
